0370a4821cd088ce6085873da56da41d1766b375
Adri267340 <adriana.gonzalez@sofka.com.co>
2025-09-18 13:45:27 -0500

    Agregar CuentaBancariaController

---
+++ src/main/java/com/bank/management/controller/CuentaBancariaController.java
@@ -0,0 +1,44 @@
+package com.bank.management.controller;
+
+import com.bank.management.dto.CuentaRequestDTO;
+import com.bank.management.dto.CuentaResponseDTO;
+import com.bank.management.service.CuentaBancariaService;
+import com.bank.management.service.CuentaBancariaServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cuentas")
+public class CuentaBancariaController {
+
+    private final CuentaBancariaService cuentaBancariaService;
+
+    public CuentaBancariaController(CuentaBancariaService cuentaBancariaService) {
+        this.cuentaBancariaService = cuentaBancariaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CuentaResponseDTO> crearCuenta(@RequestBody CuentaRequestDTO dto) {
+        return ResponseEntity.ok(cuentaBancariaService.guardar(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CuentaResponseDTO>> obtenerTodas() {
+        return ResponseEntity.ok(cuentaBancariaService.obtenerTodas());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CuentaResponseDTO> obtenerPorId(@PathVariable Long id) {
+        CuentaResponseDTO cuenta = cuentaBancariaService.obtenerPorId(id);
+        return (cuenta != null) ? ResponseEntity.ok(cuenta) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarCuenta(@PathVariable Long id) {
+        cuentaBancariaService.eliminar(id); // hay que agregar este método en el service
+        return ResponseEntity.noContent().build();
+    }
+}
