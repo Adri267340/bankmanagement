04acd9dbcbc32f0f081e41d68c74c5b556639c19
Adri267340 <adriana.gonzalez@sofka.com.co>
2025-09-19 07:25:53 -0500

    Modifique dos lineas en CuentaBancaria

--- src/main/java/com/bank/management/entity/CuentaBancaria.java
+++ src/main/java/com/bank/management/entity/CuentaBancaria.java
@@ -15,20 +15,19 @@ import java.util.List;
 @AllArgsConstructor
 @ToString(exclude = "transacciones")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-
 public class CuentaBancaria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
 
-    @NotBlank(message = "El numero de cuenata es obligatorio")
+    @NotBlank(message = "El numero de cuenta es obligatorio")
     @Size(min = 10, max = 20, message = "El numero de cuenta debe tener entre 10 y 20 caracteres")
     @Column(name = "numero_cuenta", nullable = false, unique = true, length = 20)
     private String numeroCuenta;
 
     @NotNull(message = "El saldo no puede ser nulo")
-    @DecimalMin(value = "o.o", inclusive = true, message = "El saldo no puede ser negativo")
+    @DecimalMin(value = "0.0", inclusive = true, message = "El saldo no puede ser negativo")
     @Column(nullable = false)
     private BigDecimal saldo = BigDecimal.ZERO;
 
@@ -38,6 +37,4 @@ public class CuentaBancaria {
 
     @OneToMany(mappedBy = "cuenta", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Transaccion> transacciones;
-
-
 }
