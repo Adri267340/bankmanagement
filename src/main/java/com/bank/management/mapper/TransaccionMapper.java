0032750137a6c453fd446c0afc12516d5551a9b1
Adri267340 <adriana.gonzalez@sofka.com.co>
2025-09-22 21:53:18 -0500

    Agregue estas 2 lineas a TransaccionMapper TransaccionResponseDTO toDto(Transaccion transaccion);                                                void updateEntity(TransaccionRequestDTO dto, @MappingTarget Transaccion transaccion);

--- src/main/java/com/bank/management/mapper/TransaccionMapper.java
+++ src/main/java/com/bank/management/mapper/TransaccionMapper.java
@@ -5,14 +5,15 @@ import com.bank.management.dto.TransaccionResponseDTO;
 import com.bank.management.entity.Transaccion;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface TransaccionMapper {
 
     @Mapping(source = "cuentaId", target = "cuenta.id")
     Transaccion toEntity(TransaccionRequestDTO dto);
-
     @Mapping(source = "cuenta.id", target = "cuentaId")
     TransaccionResponseDTO toDto(Transaccion transaccion);
+    void updateEntity(TransaccionRequestDTO dto, @MappingTarget Transaccion transaccion);
 }
 
