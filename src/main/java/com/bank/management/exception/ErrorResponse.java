26c047aa3d5afad391dd9a854fe51b3123c52a93
Adri267340 <adriana.gonzalez@sofka.com.co>
2025-09-29 20:04:43 -0500

    Agregue ErrorResponse.java

---
+++ src/main/java/com/bank/management/exception/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.bank.management.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+}
+
+
+
+
